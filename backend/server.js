d0d79f4351fb65055ae16a0972e9da5aaf7a8593
Bryce Keeton <brycekeetonazaz@gmail.com>
2020-12-01 06:16:34 -0800

    did what I could

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,122 @@ const express = require("express"),
 app = express(),
 port = process.env.PORT || 5000,
 cors = require("cors");
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 
 app.use(cors());
 app.listen(port, () => console.log("Backend server live on " + port));
 
+let db = new sqlite3.Database('./data/data.sqlite', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+});
+
+var dataArr1 = [];
+var dataArr2 = [];
+var dataArrWS = [];
+var dataArrField = [];
+
+db.serialize(() => {
+
+    db.all("SELECT * FROM Ag", function(err, rows) {
+        rows.forEach(function(row) {
+            dataArr1.push({
+                "farm": row.Farm,
+                "field": row.Field,
+                "crop": row.Crop,
+                "activity": row.Activity_Name,
+                "dateDue": row.Date_Due,
+                "startTime": row.Start_Time, 
+                "stopTime": row.End_time,
+                "activeIngredient": row.Active_Ingredient,
+                "cumulativeArea": row.Cumulative_Area_acre,
+                "totalApplicationRate": row.Total_Application_Rate_gal_per_acre,
+                "volumnRate": row.Volume_Rate_gal_per_acre,
+                "harvestedWeight": row.Harvested_Weight_ton
+            })
+        });
+
+        // fs.writeFile('data.json', JSON.stringify(dataArr), (err) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        // });
+    });
+
+    db.all("SELECT * FROM Wise", function(err, rows) {
+        rows.forEach(function(row) {
+            dataArr2.push({
+                "time": row.Time,
+                "canal": row.Canal_Level_PSI,
+                "flow": row.Flow_Meter,
+                "rain": row.Rain_Meter,
+                "sentek": row.Sentek_1_sensor_depth,
+                "solar": row.Solar_Radiation, 
+                "temp": row.Temp,
+                "wind": row.Wind_Direction,
+            })
+        });
+    });
+    
+    // MAKE THE AGWORLD DATES COMPATIBLE TO SQLITE BECAUSE M/D/Y FORMAT IS FOR PLEBS
+		db.all("CREATE VIEW agWorkableDates AS SELECT date(y || '-' || m || '-' || d) as date, * FROM (SELECT Date_Due, (cast(substr(Date_Due, -1, -3) as INTEGER) * 10 + cast(substr(Date_Due, -1, 1) as INTEGER)) as y, CASE WHEN length('0' || (cast(substr(Date_Due, 0, 3) as INTEGER))) == 2 THEN '0' || (cast(substr(Date_Due, 0, 3) as INTEGER)) ELSE (cast(substr(Date_Due, 0, 3) as INTEGER)) END m, CASE WHEN length('0' || (cast(substr(Date_Due, 3, 4) as INTEGER) | cast(substr(Date_Due, 4, 4) as INTEGER))) == 2 THEN ('0' || (cast(substr(Date_Due, 3, 4) as INTEGER) | cast(substr(Date_Due, 4, 4) as INTEGER))) ELSE (cast(substr(Date_Due, 3, 4) as INTEGER) | cast(substr(Date_Due, 4, 4) as INTEGER)) END d, * FROM AgWorld);", (err) => {
+			if (err) {
+			  return console.error(err.message);
+			}
+			console.log('Created view.');
+		});
+    
+    // QUERY FOR DAILY SCHEDULE
+	db.all("SELECT * FROM (SELECT date, Farm, Field, Crop, Activity_Name FROM agWorkableDates WHERE abs(julianday('now') - julianday(date)) < 8 and julianday('now') <= julianday(date) UNION SELECT substr(Time, 0, 11) as date, ' . ' as Farm, ' . ' as Field, ' . ' as Crop, 'WiseConn Irrigation' as Activity_Name FROM Wise WHERE abs(julianday('now') - julianday(substr(Time, 0, 11))) < 8 and julianday('now') <= julianday(substr(Time, 0, 11)) GROUP BY julianday(substr(Time, 0, 11))) ORDER BY date asc;", function(err, rows) {
+		rows.forEach(function(row) {
+			dataArrWS.push({
+				"date": row.date,
+				"farm": row.Farm,
+				"field": row.Field,
+				"crop": row.Crop,
+				"activity": row.Activity_Name
+			})
+		});
+	});
+	
+	// QUERY FOR THE FIELDS
+	db.all("SELECT distinct Farm, Crop FROM Ag;", function(err, rows) {
+		rows.forEach(function(row) {
+			dataArrField.push({
+				"farm": row.Farm,
+				"crop": row.Crop
+			})
+		});
+	});
+
+});
+
+// close the database connection
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+app.get("/agworld", (req, res) => {
+    res.send(JSON.stringify(dataArr1));
+});
+
+app.get("/wiseconn", (req, res) => {
+    res.send(JSON.stringify(dataArr2));
+});
+
+app.get("/schedule", (req, res) => {
+	res.send(JSON.stringify(dataArrWS));
+});
+app.get("/fields", (req, res) => {
+	res.send(JSON.stringify(dataArrField));
+});
+
 app.get("/", (req, res) => {
-res.send({ message: "We did it!" });
+    res.send(res.json({ data: 'test' }));
 });
--- veracruz-ventures/src/pages/Dashboard.js
+++ veracruz-ventures/src/pages/Dashboard.js
@@ -1,34 +1,144 @@
-import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-const sqlite3 = require('sqlite3').verbose();
+import React, {useState} from 'react';
+import MUIDataTable from "mui-datatables";
+import { makeStyles } from "@material-ui/core/styles";
+  import { Chart } from "frappe-charts";
 
-const Dashboard = () => {
-    
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  flexGrow: 1,
+	  backgroundColor: theme.palette.background.paper,
+	  width: '100%',
+	  position: 'relative'
+	}
+}));
 
-    // open the database
-    let db = new sqlite3.Database('./data/practicedata.sqlite');
-
-    let sql = `SELECT * FROM AgWorldPractice`;
-
-    db.all(sql, [], (err, rows) => {
-    if (err) {
-        throw err;
-    }
-    rows.forEach((row) => {
-        return <div>row.farm + "\t" + row.fieldID</div>;
-        });
-    });
-    
-
-// close the database connection
-db.close();
-
-    
+const options = {
+    filterType: 'checkbox',
+    selectableRows: 'none',
+    print: 'false',
+    tableBodyHeight: '20%',
+    tableBodyMaxHeight: '20%',
+    //ask the industry partner about it
+    //download: 'false' 
 };
 
+const columns = [
+    {   name: "date",
+        label: "Date Due",
+    },
+    {   name: "farm",
+        label: "Farm",
+    },
+    {   name: "field",
+        label: "Field",
+    },
+    {
+		name: "crop",
+		label: "Crop",
+	},
+    {   name: "activity",
+        label: "Activity",
+    }
+];
+
+const fieldColumns = [
+    {   name: "farm",
+        label: "Farm",
+    },
+    {
+		name: "crop",
+		label: "Crop",
+	}
+];
+
+
+const Dashboard = () => {
+    const [rows, setRows] = React.useState([]);
+    //const [fRows, setFRows] = React.useState([]);
+    const [agRows, setAgRows] = React.useState([]);
+    const [tabIndex, setTabIndex] = useState(0);
+    const classes = useStyles();
+
+    const handleGetAgData = async() => {
+        const responseAg = await fetch("http://localhost:5000/agworld");
+        setAgRows(await responseAg.json());
+        return await responseAg.json;
+    };
+    const handleGetData = async() => {
+        const response = await fetch("http://localhost:5000/schedule");
+        setRows(await response.json());
+        return await response.json;
+    };
+    /*const handleFieldData = async() => {
+        const responseF = await fetch("http://localhost:5000/fields");
+        setFRows(await responseF.json());
+        return await responseF.json;
+	};*/
+
+    const myRows = handleGetData();
+    //const myFRows = handleFieldData();
+    const myAgRows = handleGetAgData();
+    const scheduleData = (value) => {
+		
+	}
+    
+    //const dataAg = JSON.parse(myAgRows);
+    
+	/*const dataToUse = {
+		labels: "field",
+		datasets: [
+        {
+            name: "Some Data", type: "bar",
+            values: [25, 40, 30, 35, 8, 52, 17, -4]
+        }]
+	}
+	const chart = new Chart("#chart", {  // or a DOM element,
+		// new Chart() in case of ES6 module with above usage
+		title: "My Awesome Chart",
+		data: dataToUse,
+		type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+		height: 250,
+		colors: ['#7cd6fd', '#743ee2']
+	});*/
+    
+/*<MUIDataTable
+        title={"Browse List"}
+        data={rows}
+        columns={columns}
+        options={options}
+        
+        /> 
+        * <chart />
+					<MUIDataTable
+						title={"Graphs"}
+						data={fRows}
+						columns={fieldColumns}
+						options={options}
+					/>*/
+	
+    const formatTable = {
+		display: "block",
+		marginLeft: "auto",
+		marginRight: "auto"
+	}
+    return (
+	   <div>
+			<table style={formatTable}><tr>
+			<td>
+		   <div>
+				 <iframe width="500" height="400" frameborder="0" src="https://www.bing.com/maps/embed?h=400&w=500&cp=37.34211233036821~-120.47395737423523&lvl=16&typ=d&sty=h&src=SHELL&FORM=MBEDV8" scrolling="no">
+				 </iframe>
+			</div></td><td>
+		   <MUIDataTable
+				title={"Weekly Schedule"}
+				data={rows}
+				columns={columns}
+				options={options}
+			/></td></tr></table>
+		</div>
+    );
+};
+
+
 export default Dashboard;
-
-
-
-
 
